bd7cb180b507190ea287f672048f6e273c613a2c
Wassim-26 <abderahmanewassim652@gmail.com>
2025-03-08 10:39:27 +0100

    add edition option

--- src/pages/Sourates/Sourates.jsx
+++ src/pages/Sourates/Sourates.jsx
@@ -1,24 +1,44 @@
 import { useState, useEffect } from "react";
 import "./Sourates.css";
+import { useParams } from "react-router";
 export default function Sourates() {
   const [surah, setsurah] = useState([]);
-  const [loading, setloading] = useState(true);
-
+  const [Edition, setEdition] = useState([]);
+  const [LoadingEdition, setLoadingEdition] = useState(true);
+  const [LoadingSurah, setLoadingsurah] = useState(true);
+  const [CurrentEdition, setCurrentEdition] = useState("ar.alafasy");
+  const params = useParams();
   useEffect(() => {
-    fetch("https://api.alquran.cloud/v1/surah/5/ar.alafasy")
+    fetch(
+      `https://api.alquran.cloud/v1/surah/${params.numberSourat}/${CurrentEdition}`
+    )
       .then((response) => response.json())
       .then((data) => {
         setsurah(data.data);
-        setloading(false);
+        setLoadingsurah(false);
       });
-  }, []);
+    fetch("https://api.alquran.cloud/v1/edition")
+      .then((response) => response.json())
+      .then((data) => {
+        setEdition(data.data);
+        setLoadingEdition(false);
+      });
+  }, [CurrentEdition]);
 
-  console.log(surah);
-  if (loading) return <div>loading...</div>;
+  if (LoadingSurah || LoadingEdition) return <div>loading...</div>;
 
   return (
     <div className="sourate" dir="rtl">
       <h1>- {surah.name} - </h1>
+      <select
+        onChange={(event) => {
+          setCurrentEdition(event.target.value);
+        }}
+      >
+        {Edition.map((edition) => (
+          <option value={edition.identifier}> {edition.name}</option>
+        ))}
+      </select>
       <div className="ayat-container">
         {surah.ayahs.map((aya) => (
           <div className="aya">
@@ -27,6 +47,7 @@ export default function Sourates() {
               src={`https://cdn.islamic.network/quran/images/${surah.number}_${aya.numberInSurah}.png`}
               alt="ayat"
             />
+            <p>{aya.text}</p>
           </div>
         ))}
       </div>
