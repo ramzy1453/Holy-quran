68f9818c7cf947ea739f9ace28b3b93aca8fc7b2
Wassim-26 <abderahmanewassim652@gmail.com>
2025-03-15 10:44:25 +0100

    reciters page

--- src/pages/Reciters/reciter.jsx
+++ src/pages/Reciters/reciter.jsx
@@ -16,24 +16,15 @@ export default function Reciter() {
   if (loading) return <div className="loader">loading...</div>;
   return (
     <div className="reciter-container">
-      {" "}
       <h1> Quran Reciters</h1>
-      {reciter.slice(0, 6).map((reciters) => (
-        <div className="reciter-card">
-          <div className="reciter-img">
-            <img
-              src="https://ilmfeed.com/wp-content/uploads/2017/07/yasser-al-dosari.jpg"
-              alt="reciter"
-              width={72}
-              height={72}
-            />
-          </div>
-          <div className="reciter-details">
+      <div className="reciter-cards-container">
+        {reciter.map((reciters) => (
+          <div className="reciter-card">
             <h2>{reciters.englishName}</h2>
             <p>{reciters.name}</p>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
